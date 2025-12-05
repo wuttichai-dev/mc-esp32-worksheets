2b6cf17687a0436c37517d58e47da7e23410d0e2
MBP-2013 <mbp-2013@MBP-2013s-MacBook-Pro.local>
2025-12-05 19:21:22 +0700

    readd lab2_4

--- lab02/lab2_4.py
+++ lab02/lab2_4.py
@@ -1,11 +1,11 @@
 from machine import Pin
 import time
 
-lr = Pin(34, Pin.IN)
+lr1 = Pin(34, Pin.IN)
 led = Pin(16, Pin.OUT)
 
 while True:
-    if lr.value() == 0:
+    if lr1.value() == 0:
         led.on()
     else:
         led.off()
